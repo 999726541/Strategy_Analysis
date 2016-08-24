e43e96a757416981499e460ba0236a9ce7d05b45
999726541 <leo.tao@icloud.com>
2016-08-24 13:50:35 +0800

    2016-08-22/momentum strategy research

--- Strategys/ma_one_varible/runner.py
+++ Strategys/ma_one_varible/runner.py
@@ -1,6 +1,10 @@
 # -*- coding:utf-8 -*-
 
-from Strategys.ma_one_varible.one_ma_factorial import test_
+import datetime
+
+import pandas as pd
+
+from Strategys.ma_one_varible.one_ma_factorial import test_,hs_300_strategy
 from pitcher import get_his_data,write_to_mongo
 
 
@@ -22,7 +26,7 @@ def fixed_90():
 
 #--------------------------------increased_days_ma-----------------
 
-def date_incresed():
+def date_accmulation():
     ma = [i + 1 for i in range(120)]
     print(ma)
     df_all = get_his_data('hs300', ma=ma)
@@ -33,9 +37,22 @@ def date_incresed():
         end = df.index[len(df) - 1]
         print('start calculate :' + str(start) + ' to ' + str(end))
         rr = test_(ma, df)
-        write_to_mongo(rr, db='MA_testback', collection='Single_MA_90_increase', id=str(end))
+        write_to_mongo(rr, db='MA_testback', collection='Single_MA_accmulation', id=str(end))
 
+# ----------------------------------------最近日,所有均线策略合集----------------
+def date_accmulation_getall():
+    ma = [i  for i in range(2,241)]
+    df_all = get_his_data('hs300', ma=ma)
+    test = hs_300_strategy(df_all)
+    for ii in ma:
+        print('processing MA_'+str(ii))
+        test.backtest(ma_1=str(ii),ma_2=str(ii))
+        # return原始是1,100%
+        df = pd.concat([test._df.position,test._df['return'],test._df.beta,test._df.drawdown,test._df.long,
+                        test._df.short],axis=1)
+        write_to_mongo(df, db='MA_testback', collection='Single_MA_accmulation_all', id='MA_'+str(ii)+'_'+str(datetime.datetime.today())[:10])
+        test = hs_300_strategy(df_all)
 
 #---------------------------Runner-----------------------
 
-fixed_90()
+date_accmulation_getall()
